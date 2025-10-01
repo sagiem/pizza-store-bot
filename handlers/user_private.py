64fa8b025f166ea762873e72a8d3d1f02e21576a
Maksim Kashin <sagiems@gmail.com>
2025-10-01 16:32:55 +0600

    база данных

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -5,7 +5,9 @@ from aiogram.utils.formatting import (
     as_list,
     as_marked_section,
     Bold,
-)  # Italic, as_numbered_list и тд
+)
+from sqlalchemy.ext.asyncio import AsyncSession
+from database.orm_query import orm_get_products  # Italic, as_numbered_list и тд
 
 from filters.chat_types import ChatTypeFilter
 
@@ -32,7 +34,13 @@ async def start_cmd(message: types.Message):
 
 # @user_private_router.message(F.text.lower() == "меню")
 @user_private_router.message(or_f(Command("menu"), (F.text.lower() == "меню")))
-async def menu_cmd(message: types.Message):
+async def menu_cmd(message: types.Message, session: AsyncSession):
+    for product in await orm_get_products(session):
+        await message.answer_photo(
+            product.image,
+            caption=f"<strong>{product.name}\
+                    </strong>\n{product.description}\nСтоимость: {round(product.price, 2)}",
+        )
     await message.answer("Вот меню:")
 
 
@@ -58,7 +66,7 @@ async def payment_cmd(message: types.Message):
 @user_private_router.message(
     (F.text.lower().contains("доставк")) | (F.text.lower() == "варианты доставки"))
 @user_private_router.message(Command("shipping"))
-async def menu_cmd(message: types.Message):
+async def shipping_cmd(message: types.Message):
     text = as_list(
         as_marked_section(
             Bold("Варианты доставки/заказа:"),
